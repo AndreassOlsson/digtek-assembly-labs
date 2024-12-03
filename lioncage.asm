076238e50c18203e14b450cfa340bbafe50d3864
Andreas Olsson <andreas.o.olsson@outlook.com>
2024-12-03 12:33:39 +0100

    Add working lion cage

--- lioncage.asm
+++ lioncage.asm
@@ -1,33 +1,20 @@
-addi x10, x0, 4
-addi x11, x0, 12
-addi x12, x0, 8
+addi x1, x0, 1
+addi x3, x0, 4
+addi x4, x0, 8
 
-loop:
-    beq x31, x10, add
-    beq x31, x12, in
-    beq x0, x0, loop
+SO:
+    beq x31, x0, s1
+    bne x31, x0, s0
 
-add:
+S1:
+    beq x31, x3, OUTPUT1
+    beq x31, x4, OUTPUT2
+    bne x31, x4, S1
+
+OUTPUT1:
     addi x30, x30, 1
-    beq x0, x0, out
+    beq x31, x3, S0
 
-out:
-    beq x31, x11, mid
-    beq x31, x0, sub
-    beq x31, x12, sub
-    beq x0, x0, out
-
-mid:
-    beq x31, x12, in
-    beq x31, x10, out
-    beq x31, x0, loop
-    beq x0, x0, mid
-
-sub:
-    addi x30, x30, -1
-    beq x0, x0, loop
-
-in:
-    beq x31, x11, mid
-    beq x31, x0, loop
-    beq x0, x0, in
+OUTPUT2:
+    sub x30, x30, x1
+    beq x31, x4, S0
